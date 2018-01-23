6a17c6f17d90e0b91d2ea5160bd5ba535cc17e29
Monty-code <33160508+Monty-code@users.noreply.github.com>
2018-01-23 19:16:56 +0100

    aggiunte nuove risposte

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,5 @@
 function mago(){
-  var randomNumber = Math.floor(Math.random() * 16);
+  var randomNumber = Math.floor(Math.random() * 40);
   var eightBall = "";
   switch(randomNumber){
     case 0 :
@@ -15,22 +15,22 @@ function mago(){
       eightBall = "Cerca sempre di caricarti di pensieri positivi perche\' solo cosi\' l\' universo oggi potra\' portarti fortuna!";
       break;
     case 4 :
-      eightBall = "Se qualcosa oggi andra\' storto tu ricordati che un\' angelo materno ti aiutera\' nel tuo domani e tutto svanira\'";
+      eightBall = "Se qualcosa oggi andra\' storto tu ricordati che un\' angelo materno ti aiutera\' nel tuo domani e tutto svanira\'.";
       break;
     case 5 :
-      eightBall = "A volte serve sbattere la testa per imparare a non rompersela...gli angeli medicheranno ogni tua ferita, ma segui il loro insegnamento";
+      eightBall = "A volte serve sbattere la testa per imparare a non rompersela...gli angeli medicheranno ogni tua ferita, ma segui il loro insegnamento.";
       break;
     case 6 :
-      eightBall = "Oggi potrebbe essere una giornata molto speciale e da ricordare in positivo o negativo l'importante dicono gli angeli e\' imparare";
+      eightBall = "Oggi potrebbe essere una giornata molto speciale e da ricordare in positivo o negativo l'importante dicono gli angeli e\' imparare.";
       break;
     case 7 :
       eightBall = "Solo se saprai sorridere anche a chi magari non lo merita molto, riuscirai ad avere la vendetta migliore...la pace interiore!";
       break;
     case 8 :
-      eightBall = "Oggi prendi la vita con più calma tanto cio\' che dovra\' essere sara\' comunque...un saluto da un angioletto bimbo ti aiutera\' ad essere meno duro con te stesso";
+      eightBall = "Oggi prendi la vita con più calma tanto cio\' che dovra\' essere sara\' comunque...un saluto da un angioletto bimbo ti aiutera\' ad essere meno duro con te stesso.";
       break;
     case 9 :
-      eightBall = "Oggi potresti imparare che la vita non e\' mai scontata, e\' sempre un regalo per questo bisogna viverla a piena anima e cuore! ";
+      eightBall = "Oggi potresti imparare che la vita non e\' mai scontata, e\' sempre un regalo per questo bisogna viverla a piena anima e cuore!";
       break;
     case 10 :
       eightBall = "Una splendida notizia oggi potera\' il sole a scaldare i tuoi pensieri...";
@@ -39,7 +39,7 @@ function mago(){
       eightBall = "Un messaggio inaspettato, una novita\' oggi potrebbe dare un senso a questo periodo, e\' una luce in fondo al tunnel.";
       break;
     case 12 :
-      eightBall = "Solo con l\' amore e la pazienza che oggi dovrai cercare di avere riuscirai a superare alcuni ostacoli";
+      eightBall = "Solo con l\' amore e la pazienza che oggi dovrai cercare di avere riuscirai a superare alcuni ostacoli.";
       break;
     case 13 :
       eightBall = "Oggi potresti sentire la necessita\' di avere un cambiamento nella tua vita, ricordati sempre che sai cosa lasci ma non sai cosa trovi!";
@@ -52,6 +52,75 @@ function mago(){
       break;
     case 16 :
       eightBall = "Oggi piu\' che mai hai bisogno di una mano sulla spalla che un angelo amico non manchera\' di metterti, ricorda che il sole esiste per tutti!";
+      break;
+    case 17 :
+      eightBall = "La vita a volte da\' risposte che non vogliamo sentire ma se guardi dentro di te le sapevi gia\'.";
+      break;
+    case 18 :
+      eightBall = "Se e\' amore lo scoprirai presto...gli angeli ti mostreranno dei segnali e tu capirai.";
+      break;
+    case 19 :
+      eightBall = "La risposta e\' si\'...un angelo con la lettera M ti ha portato il suo aiuto.";
+      break;
+    case 20 :
+      eightBall = "La verità\' e\' che tu non sei ancora pronta per volare...c\'e\' un tempo per ogni cosa...dattene ancora un po\'.";
+      break;
+    case 21 :
+      eightBall = "Un angelo con la letter L vuole assicurarti che andra\' tutto bene...credi in te stessa!";
+      break;
+    case 22 :
+      eightBall = "Un nuovo lavoro o spostamento potrebbe portarti nuove cose e nuove rose.";
+      break;
+    case 23 :
+      eightBall = "La vita toglie ma la vita da\'... e sta per arrivare il momento di ricevere anche per te...";
+      break;
+    case 24 :
+      eightBall = "Ecco che finalmente dopo aver tanto seminato e\' arrivato il momento di raccogliere...";
+      break;
+    case 25 :
+      eightBall = "Se riuscirai a guardare dentro di te e ad apprezzare tutto cio\' che hai...capirai che la felicita\' e\' dove la pace sta\'.";
+      break;
+    case 26 :
+      eightBall = "Un lieto evento familiare portera\' la pace interiore in ogni soggetto che la meritera\'.";
+      break;
+    case 27 :
+      eightBall = "A volte le cose fanno dei giri immensi ma...poi...ritornano.";
+      break;
+    case 28 :
+      eightBall = "La vita e\' una splendida partita e questa volta l\'avrai vinta tu...";
+      break;
+    case 29 :
+      eightBall = "Perdona non perche\' essi meritano il perdono ma perche\' tu...meriti la pace interiore.";
+      break;
+    case 30 :
+      eightBall = "La vita e\' una sola, tenta...sorridi, sogna...solo cosi\' potrai realizzare cio\' che vuoi...";
+      break;
+    case 31 :
+      eightBall = "E quando pensi che sia finita...e\' proprio allora che comincia la partita...perche' la vita sara\' una fantastica risposta positiva.";
+      break;
+    case 32 :
+      eightBall = "Non tutto cio\' di negativo che accade lo e\'...a volte certe persone escono dalla nostra vita per far arrivare il meglio.";
+      break;
+    case 33 :
+      eightBall = "Una novita\' lavorativa portera\' buone cose.";
+      break;
+    case 34 :
+      eightBall = "Cambiamenti e spostamenti...viaggi e cose nuove porteranno una lieta gioia.";
+      break;
+    case 35 :
+      eightBall = "Un angelo giovane ti ha voluto tanto bene ma vorrebbe che tu ora potessi cercare finalmente di rinascere per te e per chi come lui tiene a te.";
+      break;
+    case 36 :
+      eightBall = "Una nonnina da lassu\' ti manda una notizia di gioia col fiocco rosa.";
+      break;
+    case 37 :
+      eightBall = "Una mamma che non smetterà mai di esserlo..da lassu\' ti fa sapere che andra\' tutto bene.";
+      break;
+    case 38 :
+      eightBall = "Gioisci ogni giorno delle cose belle che hai e credi nei sogni...solo cosi\' entro breve accadranno.";
+      break;
+    case 39 :
+      eightBall = "Un angelo nonno ti e\' vicino per farti sapere che andra\' tutto bene.";
       break;
     }
 return eightBall
